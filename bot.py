4a98ae049f39eabfbfbd484add734ead876ad575
timur <timuroka2008@gmail.com>
2024-05-20 23:36:58 +0300

    g

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
-from config import BOT_TOKEN
-from dap import *
+from config import *
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -12,27 +11,18 @@ def buttons(task):
         markup.add(i)
     return markup
 
-markup_no = types.ReplyKeyboardRemove()
 
+markup_no = types.ReplyKeyboardRemove()
 
 @bot.message_handler(commands=['start'])
 def handler_start(message):
     name = message.chat.first_name
-    bot.send_message(message.chat.id, f'<b>Привет, {name}\n</b>'
-                                                      f'<i>Я Бот, в котором можно выбрать\n'
-                                                      f'удобную систему планирования и\n'
-                                                      f'контролировать свои задачи</i>',
-                                     parse_mode='html', reply_markup=buttons(["/plan", "/plan_add"]))
+    msg = bot.send_message(message.chat.id, f'<b>Привет, {name}\n</b>'
+                                                      f'<i>я бот, в котором можно выбрать удобную\n'
+                                                      f'систему планирования и контролировать свои задачи</i>',
+                                     parse_mode='html', reply_markup=buttons(menu))
+    bot.register_next_step_handler(msg, menu_go)
 
-@bot.message_handler(commands=['/plan_add'])
-def add_plan(message):
-    name = message.chat.first_name
-    bot.send_message(message.chat.id, "Выберите систему планирования)", reply_markup=buttons(Systems_plan.keys()))
-    bot.register_next_step_handler(message, adding)
 
-def adding(message):
-    if message not in Systems_plan.keys():
-        bot.send_message(message.chat.id, 'Выберите корректный ответ')
-        bot.register_next_step_handler(message, adding)
-    else:
-        if
+def menu_go(message):
+    pass
