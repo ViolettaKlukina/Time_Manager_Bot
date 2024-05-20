a1edd810cecb21589fe02aa41ae8f0732c1998ce
timur <timuroka2008@gmail.com>
2024-05-20 23:36:24 +0300

    g

--- dap.py
+++ dap.py
@@ -6,18 +6,4 @@ gtd_buttons = ['новая', 'подзадача', 'конец']
 
 
 
-
-def gtd(message):
-    bot.send_message(message.chat.id, "Введите задачу", reply_markup= buttons(gtd_buttons))
-    bot.register_next_step_handler(message, gtd_go)
-
-
-def gtd_go(message):
-    if message.text == gtd_buttons[0]:
-        gtd(message)
-
-def kanban(message):
-    pass
-
-
 Systems_plan = {'GTD': gtd, 'КАНБАН': kanban, 'МАТРИЦА ЭЙЗЕНХАУЭРА': ''}
