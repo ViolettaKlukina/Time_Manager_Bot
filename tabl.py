8d69a4adc1f14879c96a0c6f815edc7374ff17e0
timur <timuroka2008@gmail.com>
2024-05-22 17:44:38 +0300

    g

--- tabl.py
+++ tabl.py
@@ -1,35 +1,11 @@
-import xlsxwriter
 from spire.xls import *
 from spire.xls.common import *
-import jpype
-import asposecells
+import pandas as pd
+import seaborn as sns
 
-jpype.startJVM()
-import asposecells.api
-from db import *
+
 def kanban_tabl():
-    rows = ['ааааааааааааааааа', 'иииииииииииии', 'сссссссссссссссс']
-    data = [('Сделано', rows[0]), ('Делается', rows[1]), ('Надо сделать', rows[2])]
-    workbook = xlsxwriter.Workbook('Шаблон Канбан.xlsx')
-    worksheet = workbook.add_worksheet()
-    for row, (item, price) in enumerate(data):
-        worksheet.write(row, 0, item)
-        worksheet.write(row, 1, price)
-    workbook.close()
-
-    """
-    imgOptions = asposecells.api.ImageOrPrintOptions
-    imgOptions.setSaveFormat(asposecells.api.SaveFormat.SVG)
-    sheet = asposecells.api.workbook.getWorksheets().get(0)
-
-    # create sheet render object
-    sr = asposecells.api.SheetRender(sheet, imgOptions)
-
-    # convert sheet to PNG image
-    for j in range(0, sr.getPageCount()):
-        sr.toImage(j, "WorksheetToImage-out%s" % (j) + ".png")
-    """
-
-
-kanban_tabl()
+    pd.set_option('max_rows', 5)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.float_format', '{:.2f}'.format)
 
