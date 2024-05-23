3dc6e2f914d1495119e3298f436cc75422337bdf
maryewwe <152085896+maryewwe@users.noreply.github.com>
2024-05-23 15:55:32 +0300

    Add files via upload

---
+++ speechkit.py
@@ -0,0 +1,29 @@
+import requests
+from yandex_gpt import get_creds
+from config import *
+
+iam_token, folder_id = get_creds()
+
+
+def stt(data):
+    params = '&'.join([
+        'topic=general',
+        f'folder_id={folder_id}',
+        'lang=ru-RU'
+    ])
+
+    headers = {
+        'Authorization': f'Bearer {iam_token}'
+    }
+
+    response = requests.post(
+        f'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?{params}',
+        headers=headers,
+        data=data
+    )
+
+    decoded_data = response.json
+    if decoded_data('error_code') is None:
+        return True, decoded_data('result')
+    else:
+        return False, 'При запросе в SpeechKit возникла ошибка.'
