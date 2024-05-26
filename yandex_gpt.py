7176fe24ed622e94a03842071b525b9d1e209f35
maryewwe <152085896+maryewwe@users.noreply.github.com>
2024-05-26 12:32:43 +0300

    Update yandex_gpt.py

--- yandex_gpt.py
+++ yandex_gpt.py
@@ -106,20 +106,22 @@ def ask_gpt(messages):
             "temperature": 0.7,
             "maxTokens": MAX_TOKENS
         },
-        "messages": SYSTEM_PROMPT + messages
+        "messages": [
+            {
+                'role': 'user',
+                'text': messages.text
+            }
+        ]
     }
     try:
         response = requests.post(url, headers=headers, json=data)
         # проверяем статус код
         if response.status_code != 200:
             return False, f"Ошибка GPT. Статус-код: {response.status_code}", None
-        elif response.status_code != 403:
-            create_new_token()
-            return ask_gpt(messages)
         # если всё успешно - считаем количество токенов, потраченных на ответ, возвращаем статус, ответ, и количество токенов в ответе
         answer = response.json()['result']['alternatives'][0]['message']['text']
-        tokens_in_answer = count_gpt_tokens([{'role': 'assistant', 'text': answer}])
-        return True, answer, tokens_in_answer
+        tokens = count_tokens_in_dialogue(answer)
+        return True, answer, tokens
     except Exception as e:
         logging.error(e)  # если ошибка - записываем её в логи
         return False, "Ошибка при обращении к GPT",  None
